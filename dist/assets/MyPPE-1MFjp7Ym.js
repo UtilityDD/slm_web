304917647c4658d090aa9440bf6f261801c618c8
UtilityDD <utility.dipankar@gmail.com>
2026-01-04 11:33:49 +0530

    update-2

---
+++ dist/assets/MyPPE-1MFjp7Ym.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-5-YDczCD.js";import{E as a}from"./EquipmentManager-jmd0_n_S.js";import"./cacheHelper-BlUM2QW1.js";import"./SaveSuccessModal-B_JW5SS1.js";const i=({user:t,setCurrentView:o,language:r})=>e.jsx(a,{type:"ppe",user:t,language:r,setCurrentView:o,showCategories:!0,showSaveButton:!0,accentColor:"orange"});export{i as default};
--- dist/assets/index-NiMyvw0p.js
+++ dist/assets/index-NiMyvw0p.js
@@ -1,4 +1,4 @@
-import{d as BQ,R as sA}from"./index-BhI1wzMZ.js";var xt={exports:{}};/*!
+import{d as BQ,R as sA}from"./index-5-YDczCD.js";var xt={exports:{}};/*!
  * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
  * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
  * Released under MIT License
