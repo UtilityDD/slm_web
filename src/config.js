9350f5f35d1e5b58e7703464e49aaeaed8228191
UtilityDD <utility.dipankar@gmail.com>
2026-01-09 07:48:09 +0530

    🔄 Update Loop Fixed

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // App Configuration
 export const APP_NAME = "SmartLineman";
 export const API_URL = import.meta.env.VITE_SUPABASE_URL;
-export const CURRENT_APP_VERSION = "1.2.1";
+export const CURRENT_APP_VERSION = "1.2.4";
